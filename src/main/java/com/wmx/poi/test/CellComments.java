7ce2ae466971fd4024e3946dd65e93e576920d0b
wangmaoxiong <2268461750@qq.com>
2020-07-13 20:22:52 +0800

    Apache POI 操作 excel

--- src/main/java/com/wmx/poi/test/CellComments.java
+++ src/main/java/com/wmx/poi/test/CellComments.java
@@ -10,7 +10,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
- * 演示如何使用excel单元格注释，Excel注释是一种文本形状，因此插入注释与在工作表中放置文本框非常相似
+ * 演示如何使用 excel 单元格注释，Excel 注释是一种文本形状，因此插入注释与在工作表中放置文本框非常相似
  *
  * @author wangMaoXiong
  * @version 1.0
@@ -46,7 +46,7 @@ public class CellComments {
         Cell cell1 = sheet.createRow(3).createCell(1);
         cell1.setCellValue(creationHelper.createRichTextString("Hello, World"));
 
-        //anchor defines size and position of the comment in worksheet
+        //定义工作表中注释的大小和位置
         ClientAnchor clientAnchor = creationHelper.createClientAnchor();
         clientAnchor.setCol1(4);
         clientAnchor.setRow1(2);
@@ -54,20 +54,19 @@ public class CellComments {
         clientAnchor.setRow2(5);
         Comment comment1 = drawingPatriarch.createCellComment(clientAnchor);
 
-        // set text in the comment
+        // 设置注释中的文本
         comment1.setString(creationHelper.createRichTextString("We can set comments in POI"));
 
-        //set comment author.
-        //you can see it in the status bar when moving mouse over the commented cell
+        //设置评论作者。
+        //将鼠标移到注释单元格上时，可以在状态栏中看到它
         comment1.setAuthor("Apache Software Foundation");
 
-        // The first way to assign comment to a cell is via Cell.setCellComment method
+        // 将注释分配给单元格的第一种方法是通过Cell.setcell注释方法
         cell1.setCellComment(comment1);
 
-        //create another cell in row 6
+        //在第7行创建另一个单元格
         Cell cell2 = sheet.createRow(6).createCell(1);
         cell2.setCellValue(36.6);
-
 
         clientAnchor = creationHelper.createClientAnchor();
         clientAnchor.setCol1(4);
@@ -75,14 +74,12 @@ public class CellComments {
         clientAnchor.setCol2(6);
         clientAnchor.setRow2(11);
         Comment comment2 = drawingPatriarch.createCellComment(clientAnchor);
-        //modify background color of the comment, only available in HSSF currently
+        //修改注释的背景颜色，目前仅在HSSF中可用
         if (workbook instanceof HSSFWorkbook) {
             ((HSSFComment) comment2).setFillColor(204, 236, 255);
         }
-
         RichTextString string = creationHelper.createRichTextString("Normal body temperature");
-
-        //apply custom font to the text in the comment
+        //将自定义字体应用于注释中的文本
         Font font = workbook.createFont();
         font.setFontName("Arial");
         font.setFontHeightInPoints((short) 10);
@@ -91,15 +88,11 @@ public class CellComments {
         string.applyFont(font);
 
         comment2.setString(string);
-        comment2.setVisible(true); //by default comments are hidden. This one is always visible.
-
+        //默认情况下，注释是隐藏的。这个总是可见的。
+        comment2.setVisible(true);
         comment2.setAuthor("Bill Gates");
 
-        /*
-         * The second way to assign comment to a cell is to implicitly specify its row and column.
-         * Note, it is possible to set row and column of a non-existing cell.
-         * It works, the comment is visible.
-         */
+        //为单元格分配注释的第二种方法是隐式指定其行和列
         comment2.setRow(6);
         comment2.setColumn(1);
 
