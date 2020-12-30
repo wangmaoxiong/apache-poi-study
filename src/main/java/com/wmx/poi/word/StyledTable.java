5818631c4896dace05ae87128e6cd733edf06741
wangmaoxiong <2268461750@qq.com>
2020-12-30 10:12:40 +0800

    在 Word 中创建一个简单的表格，带样式

--- src/main/java/com/wmx/poi/word/StyledTable.java
+++ src/main/java/com/wmx/poi/word/StyledTable.java
@@ -3,124 +3,107 @@ package com.wmx.poi.word;
 import org.apache.poi.xwpf.usermodel.*;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.*;
 
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.math.BigInteger;
 import java.util.List;
 
 /**
+ * 在 Word 中创建一个简单的表格，带样式
+ *
  * @author wangMaoXiong
  * @version 1.0
  * @date 2020/12/30 8:46
  */
 public class StyledTable {
 
-    public static void main(String[] args) {
-        try {
-            createStyledTable();
-        } catch (Exception e) {
-            System.out.println("Error trying to create styled table.");
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws Exception {
+        createStyledTable();
     }
 
-    /**
-     * Create a table with some row and column styling. I "manually" add the
-     * style name to the table, but don't check to see if the style actually
-     * exists in the document. Since I'm creating it from scratch, it obviously
-     * won't exist. When opened in MS Word, the table style becomes "Normal".
-     * I manually set alternating row colors. This could be done using Themes,
-     * but that's left as an exercise for the reader. The cells in the last
-     * column of the table have 10pt. "Courier" font.
-     * I make no claims that this is the "right" way to do it, but it worked
-     * for me. Given the scarcity of XWPF examples, I thought this may prove
-     * instructive and give you ideas for your own solutions.
-     */
     public static void createStyledTable() throws Exception {
-        // Create a new document from scratch
+        //XWPFDocument 是用于处理 .docx 文件的高级 API。创建一个新的空文档
+        XWPFDocument xwpfDocument = new XWPFDocument();
 
-        try (XWPFDocument doc = new XWPFDocument()) {
-            // -- OR --
-            // open an existing empty document with styles already defined
-            //XWPFDocument doc = new XWPFDocument(new FileInputStream("base_document.docx"));
+        // 创建一个包含6行3列的新表
+        int nRows = 6;
+        int nCols = 3;
+        XWPFTable table = xwpfDocument.createTable(nRows, nCols);
 
-            // Create a new table with 6 rows and 3 columns
-            int nRows = 6;
-            int nCols = 3;
-            XWPFTable table = doc.createTable(nRows, nCols);
+        //设置表格宽度为 100%
+        table.setWidth("100%");
 
-            // Set the table style. If the style is not defined, the table style
-            // will become "Normal".
-            CTTblPr tblPr = table.getCTTbl().getTblPr();
-            CTString styleStr = tblPr.addNewTblStyle();
-            styleStr.setVal("StyledTable");
+        // 获取表中的所有行
+        List<XWPFTableRow> rows = table.getRows();
+        int rowCt = 0;
+        int colCt = 0;
+        for (XWPFTableRow row : rows) {
+            // 获取表中行的属性（trPr）
+            CTTrPr trPr = row.getCtRow().addNewTrPr();
+            // 设置行高
+            CTHeight ht = trPr.addNewTrHeight();
+            ht.setVal(BigInteger.valueOf(360));
 
-            // Get a list of the rows in the table
-            List<XWPFTableRow> rows = table.getRows();
-            int rowCt = 0;
-            int colCt = 0;
-            for (XWPFTableRow row : rows) {
-                // get table row properties (trPr)
-                CTTrPr trPr = row.getCtRow().addNewTrPr();
-                // set row height; units = twentieth of a point, 360 = 0.25"
-                CTHeight ht = trPr.addNewTrHeight();
-                ht.setVal(BigInteger.valueOf(360));
+            // 获取行中的所有列
+            List<XWPFTableCell> cells = row.getTableCells();
+            // 向每个单元格添加内容
+            for (XWPFTableCell cell : cells) {
+                // 获取表格单元格属性元素（tcPr）
+                CTTcPr tcpr = cell.getCTTc().addNewTcPr();
+                // 将垂直对齐设置为"居中"
+                CTVerticalJc va = tcpr.addNewVAlign();
+                va.setVal(STVerticalJc.CENTER);
 
-                // get the cells in this row
-                List<XWPFTableCell> cells = row.getTableCells();
-                // add content to each cell
-                for (XWPFTableCell cell : cells) {
-                    // get a table cell properties element (tcPr)
-                    CTTcPr tcpr = cell.getCTTc().addNewTcPr();
-                    // set vertical alignment to "center"
-                    CTVerticalJc va = tcpr.addNewVAlign();
-                    va.setVal(STVerticalJc.CENTER);
+                // 创建单元格颜色元素
+                CTShd ctshd = tcpr.addNewShd();
+                ctshd.setColor("auto");
+                ctshd.setVal(STShd.CLEAR);
+                if (rowCt == 0) {
+                    //表头
+                    ctshd.setFill("A7BFDE");
+                } else if (rowCt % 2 == 0) {
+                    // 偶数行
+                    ctshd.setFill("D3DFEE");
+                } else {
+                    // 奇数行
+                    ctshd.setFill("EDF2F8");
+                }
 
-                    // create cell color element
-                    CTShd ctshd = tcpr.addNewShd();
-                    ctshd.setColor("auto");
-                    ctshd.setVal(STShd.CLEAR);
-                    if (rowCt == 0) {
-                        // header row
-                        ctshd.setFill("A7BFDE");
-                    } else if (rowCt % 2 == 0) {
-                        // even row
-                        ctshd.setFill("D3DFEE");
-                    } else {
-                        // odd row
-                        ctshd.setFill("EDF2F8");
-                    }
-
-                    // get 1st paragraph in cell's paragraph list
-                    XWPFParagraph para = cell.getParagraphs().get(0);
-                    // create a run to contain the content
-                    XWPFRun rh = para.createRun();
-                    // style cell as desired
-                    if (colCt == nCols - 1) {
-                        // last column is 10pt Courier
-                        rh.setFontSize(10);
-                        rh.setFontFamily("Courier");
-                    }
-                    if (rowCt == 0) {
-                        // header row
-                        rh.setText("header row, col " + colCt);
-                        rh.setBold(true);
-                        para.setAlignment(ParagraphAlignment.CENTER);
-                    } else {
-                        // other rows
-                        rh.setText("row " + rowCt + ", col " + colCt);
-                        para.setAlignment(ParagraphAlignment.LEFT);
-                    }
-                    colCt++;
-                } // for cell
-                colCt = 0;
-                rowCt++;
-            } // for row
-
-            // write the file
-            try (OutputStream out = new FileOutputStream("styledTable.docx")) {
-                doc.write(out);
+                // 获取单元格段落列表中的第一段
+                XWPFParagraph para = cell.getParagraphs().get(0);
+                // 创建包含内容的文本域
+                XWPFRun rh = para.createRun();
+                // 根据需要设置单元格样式
+                if (colCt == nCols - 1) {
+                    // 最后一列的字体大小与字体
+                    rh.setFontSize(10);
+                    rh.setFontFamily("Courier");
+                }
+                if (rowCt == 0) {
+                    // 表头行
+                    rh.setText("header row, col " + colCt);
+                    rh.setBold(true);
+                    para.setAlignment(ParagraphAlignment.CENTER);
+                } else {
+                    // 其它行
+                    rh.setText("row " + rowCt + ", col " + colCt);
+                    para.setAlignment(ParagraphAlignment.LEFT);
+                }
+                //局部变量 列索引加 1
+                colCt++;
             }
+            //新的一行开始时，列索引重置为0
+            colCt = 0;
+            //局部变量 行索引加 1
+            rowCt++;
         }
+
+        File homeDirectory = FileSystemView.getFileSystemView().getHomeDirectory();
+        File file = new File(homeDirectory, "Word中创建带样式的表格" + (System.currentTimeMillis()) + ".docx");
+        OutputStream os = new FileOutputStream(file);
+        xwpfDocument.write(os);
+        System.out.println("文件输出成功：" + file.getAbsolutePath());
     }
 }
